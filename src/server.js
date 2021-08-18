01a0b32d90b84ae10e61b1b5d40ab2d638239442
devjoemedia <joenart2@gmail.com>
2021-08-18 07:19:27 -0700

    added CRUD functionality

--- src/server.js
+++ src/server.js
@@ -4,7 +4,6 @@ import mongoose from 'mongoose';
 import Note from './notesModel';
 
 import Entrypoint from './views/www.marko';
-import HomePage from './views/pages/Home.marko'
 
 const Assets = require( process.env.RAZZLE_ASSETS_MANIFEST )
 
@@ -18,7 +17,76 @@ app
 .disable('x-powered-by')
 .use( markoMiddleware() ) // Enable res.marko
 .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
+app.use(express.json())
 
+
+// Get All Notes
+app.get('/api/notes', async(req, res) => {
+  try {
+    const notes = await Note.find();
+    
+    res.status(200).json({
+      message: "Success",
+      notes
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+}); 
+
+// Get Single Note
+app.get('/api/notes/:id', async(req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    res.status(200).json({
+      message: "Success",
+      note
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// Create Single Note
+app.post('/api/notes/', async(req, res) => {
+  try {  
+    const note = await Note.create(req.body);
+    res.status(200).json({
+      message: "Success",
+      note
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+});  
+
+// Update Note
+app.patch('/api/notes/:id', async(req, res) => {
+  try {  
+    const note = await Note.findByIdAndUpdate(req.params.id,req.body);
+    res.status(200).json({
+      message: "Success",
+      note
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+}); 
+
+// Delete Note
+app.delete('/api/notes/:id', async(req, res) => {
+  try {  
+    const note = await Note.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Success",
+      note
+    })
+  } catch (err) {
+    console.log(err.message);
+  }
+});  
+
+// Pages Route Handler
 app.get('/*', async(req, res) => {
   try {
     const notes = await Note.find();
@@ -32,6 +100,8 @@ app.get('/*', async(req, res) => {
     console.log(err.message);
   }
 }); 
+
+
 
 export default app;
  
