e4d11be6e4c2dbf7e82dad49e9175a1f6e1cadcb
devjoemedia <joenart2@gmail.com>
2021-08-25 03:40:26 +1200

    adding changes to contextMenu

--- src/client.js
+++ src/client.js
@@ -1,9 +1,10 @@
-import App from './views/App.marko'
-import Home from './views/pages/Home'
-import Add from './views/pages/Add'
-import Edit from './views/pages/Edit'
-import Note from './views/pages/Note'
-import NotFound from './views/pages/404'
+import App from './views/App.marko';
+import Home from './views/pages/Home';
+import Add from './views/pages/Add';
+import Edit from './views/pages/Edit';
+import Note from './views/pages/Note';
+import NotFound from './views/pages/404';
+import contextMenu from './views/components/contextMenu';
 
 const Routes = [
     { name: 'home', path: '/', component: Home },
@@ -13,23 +14,24 @@ const Routes = [
     { name: '404', path: '/not-found', component: NotFound },
 ]
 
- 
-
 // Get the custom context menu element
-const contextMenu = document.querySelector('#contextMenu');
+const contextMenuContainer = document.querySelector('#contextMenu');
 
 // Displays the custom context menu
 window.addEventListener('contextmenu', (event)=> {
     event.preventDefault();
-    contextMenu.style.top = event.offsetY + "px";
-    contextMenu.style.left = event.offsetX + "px";
-    contextMenu.style.display = "block";
+    
+    contextMenu.render({isRender: true}, (err, result) => {
+        if(result) {
+            result.appendTo(document.body)
+        }  
+    })
 
 });
 
 // Removes the context element when the document is clicked
-window.addEventListener('click', ()=> {
-    contextMenu.style.display = "none";
+window.addEventListener('click', (event)=> {
+    // contextMenuContainer.style.display = "none";
 });
 
 
