8ace5aa66c8acb3a8fbca89bbebbef1c09ee8308
devjoemedia <joenart2@gmail.com>
2021-08-26 04:14:28 +1200

    adding context option

--- src/client.js
+++ src/client.js
@@ -4,7 +4,6 @@ import Add from './views/pages/Add';
 import Edit from './views/pages/Edit';
 import Note from './views/pages/Note';
 import NotFound from './views/pages/404';
-import contextMenu from './views/components/contextMenu';
 
 const Routes = [
     { name: 'home', path: '/', component: Home },
@@ -17,21 +16,14 @@ const Routes = [
 // Get the custom context menu element
 const contextMenuContainer = document.querySelector('#contextMenu');
 
-// Displays the custom context menu
-window.addEventListener('contextmenu', (event)=> {
-    event.preventDefault();
-    
-    contextMenu.render({isRender: true}, (err, result) => {
-        if(result) {
-            result.appendTo(document.body)
-        }  
-    })
-
-});
-
+// window.addEventListener('contextmenu', (e)=> {
+//     e.preventDefault()
+// })
 // Removes the context element when the document is clicked
 window.addEventListener('click', (event)=> {
-    // contextMenuContainer.style.display = "none";
+   
+    let contextMenu = document.querySelector('#contextOptions');
+    contextMenu.style.display = "none";
 });
 
 
